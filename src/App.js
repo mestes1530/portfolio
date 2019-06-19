b5613fc416cbb703331c92fd68418e3a1d54c1d8
mitchellestes <mestes1530@gmail.com>
2019-06-19 16:38:38 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
 import './App.css';
+
+class Home extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
+
+class Contact extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Contact</h1>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="mainDiv">
+          <Router>
+            <Link to="/home">Home</Link>
+            <br></br>
+            <Link to="/contact">Contact</Link>
+            <Route path="/home" component={Home} />
+            <Route path="/contact" component={Contact} />
+          </Router>
+        </div>
       </header>
     </div>
   );
